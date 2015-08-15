29cd558a19a27f30817a5929d75bf6a42fc2ac88
srishett <srishett@cisco.com>
2015-08-15 13:24:17 +0530

    menus and chef implementation

--- webcontent/js/app.js
+++ webcontent/js/app.js
@@ -9,4 +9,24 @@
  * Main module of the application.
  */
 var app = angular
-    .module('wunderChefApp', ["ngTouch", "angucomplete"]);
+    .module('wunderChefApp', ["ngRoute","ngTouch", "angucomplete"])
+
+	.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'landingpage.html',
+        controller: 'ChefLandingDataController'
+    }).
+      when('/chefs', {
+        templateUrl: 'chefs.html',
+        controller: 'ChefMainDataController'
+      }).
+	   when('/menus', {
+        templateUrl: 'food_order.html',
+        controller: 'MenuDataController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+}]);
--- webcontent/js/app/controllers/main-controller.js
+++ webcontent/js/app/controllers/main-controller.js
@@ -1,7 +1,57 @@
-app.controller('ChefDataController', ['$scope','$http',function($scope,$http){
+app.controller('MainController', ['$scope','$http','$location',function($scope,$http,$location){
+   $scope.goNext = function (hash) { 
+   $location.path(hash);
+ }
+  }]);
+app.controller('ChefLandingDataController', ['$scope','$http',function($scope,$http){
     $scope.cheflist = [];
 		$scope.chefFormData = [];
     $http.get('http://localhost:8080/ChefWeb/ChefInfoForm.chf').success(function(data){
       $scope.chefFormData = data;
     });
+  }]);
+app.controller('ChefMainDataController', ['$scope','$http',function($scope,$http){
+   $scope.chefMainFormData = []; 
+   $http.get('http://localhost:8080/ChefWeb/ChefSearchList.chf?searchParam=ch').success(function(data){
+      $scope.chefMainFormData = data;
+    });
+  }]);
+app.controller('MenuDataController', ['$scope','$http','$location','$anchorScroll',function($scope,$http,$location, $anchorScroll){
+   $http.get('http://localhost:8080/ChefWeb/MenuSearchList.chf?searchParam=ch').success(function(data){
+      $scope.menuMainFormData = data;
+	  $scope.categoryList = $scope.menuCategory(data);
+    });
+	
+	$scope.scrollTo = function(id) {
+    var old = $location.hash();
+    $location.hash(id);
+    $anchorScroll();
+    //reset to old to keep any additional routing logic from kicking in
+    $location.hash(old);
+    };
+	var indexedMenus = [];
+    var indexedcategory = [];
+    // this will reset the list of indexed teams each time the list is rendered again
+    $scope.menuToFilter = function() {
+        indexedMenus = [];
+        return $scope.menuMainFormData;
+    }
+
+    $scope.menuItems = function(menuVal) {
+        var menuIsNew = indexedMenus.indexOf(menuVal.category) == -1;
+        if (menuIsNew) {
+            indexedMenus.push(menuVal.category);
+        }
+        return menuIsNew;
+    }
+	$scope.menuCategory = function(menuData) {
+		for(var i=0;i<menuData.length;i++)
+		{
+        var categoryIsNew = indexedcategory.indexOf(menuData[i].category) == -1;
+        if (categoryIsNew) {
+            indexedcategory.push(menuData[i].category);
+        }
+		}
+		return indexedcategory;
+    }
   }]);
